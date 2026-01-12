3c367f547faf6d3d2896b6f00f02eb5c91d3d07b
Nguyễn Nhất Huy <116622843+not-for-tomorrow@users.noreply.github.com>
2026-01-12 16:21:36 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Chu-Nghia-Xa-Hoi/'
+  base: '/chu-nghia-xa-hoi/'
 })
